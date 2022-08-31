af808fbb57823ec9dc4d4dcc07c26de506005f61
BigSiCorp <si.kocorp@gmail.com>
2022-08-31 21:27:34 +0300

    partly completed HW
    1-2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const containerForGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems.reduce(
+  (acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+    );
+  },
+  ''
+);
+
+containerForGallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on();
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let time = 0;
 
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', onPlay);
